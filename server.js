8751328da101637fd244457d91ecde18827790ba
Faris-07 <farisuddin03@gmail.com>
2023-02-22 17:08:19 +0000

    Complete Bonus Task

--- server.js
+++ server.js
@@ -4,7 +4,19 @@ const port = 3000;
 const {Restaurant, Menu, Item} = require("./models/index")
 const {sequelize} = require("./db")
 
-//TODO: 
+//TODO:
+app.use(express.json())
+app.get('/restaurants', async (req, res) => {
+    let allRestaurants = await Restaurant.findAll({
+        include: [
+            {
+                model: Menu,
+                include: Item
+            },
+        ],
+    })
+    res.json(allRestaurants);
+}) 
 
 app.listen(port, () => {
     sequelize.sync()
